312d74bf58446be182451a568f350d64889032ea
blue <blue@exp.com>
2026-04-29 04:39:27 +0300

    r9

--- E.java
+++ E.java
@@ -19,4 +19,8 @@ public class E extends K {
     public long ac() {
         return 222;
     }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.LinkedList<Integer>;
+    }
 }
